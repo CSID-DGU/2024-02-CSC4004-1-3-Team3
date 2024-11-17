74d41b83b61f470f564b303ae2f50b921a87c0c0
eminseo2003 <eminseo2003@naver.com>
2024-11-17 20:37:32 +0900

    Feat.로그인모달경로수정

--- Team3_front/front/src/pages/Login/Login.jsx
+++ Team3_front/front/src/pages/Login/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Login_modal from './Login_modal';
 
-const Login = () => {
-  return <>Login</>;
-};
+function Login() {
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setLoginModalOpen(true)}>로그인</button>
+      <Login_modal isOpen={isLoginModalOpen} onClose={() => setLoginModalOpen(false)} />
+    </div>
+  );
+}
 
 export default Login;
