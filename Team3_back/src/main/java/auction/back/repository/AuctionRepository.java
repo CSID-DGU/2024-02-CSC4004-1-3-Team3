64b3237f0a149d641f400bdf1f1f355396f1361b
Kimd0ng <kdongh0406@gmail.com>
2024-11-21 15:56:20 +0900

    :sparkles: feat: Main api init

---
+++ Team3_back/src/main/java/auction/back/repository/AuctionRepository.java
@@ -0,0 +1,14 @@
+package auction.back.repository;
+
+import auction.back.domain.Auction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    @Query("SELECT a FROM Auction a WHERE a.finishAt IS NULL ORDER BY a.startAt ASC")
+    List<Auction> findOngoingAuctionsOrderByStartAtAsc();
+}
