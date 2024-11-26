a6f664b9a26c1a216a797da4c4502a2bfa2b5c9a
eminseo2003 <eminseo2003@naver.com>
2024-11-26 23:18:26 +0900

    Feat: 작품페이지 수정

--- Team3_front/front/src/pages/ArtWork/detailcompo/WorkDescription.jsx
+++ Team3_front/front/src/pages/ArtWork/detailcompo/WorkDescription.jsx
@@ -1,44 +1,32 @@
 // src/components/WorkDescription.js
-import React, { useState, useEffect } from 'react';
-import worksData from '../../../components/works.jsx';
+import React from 'react';
+//import worksData from '../../../components/works.jsx';
 import './WorkDescription.css';
 
-function WorkDescription({ id }) {
-  const [item, setItem] = useState(null);
-
-  useEffect(() => {
-    const parsedId = parseInt(id, 10);
-    if (!isNaN(parsedId)) {
-      const foundItem = worksData[parsedId];
-      setItem(foundItem || null);
-    } else {
-      setItem(null);
-    }
-  }, [id]);
-
-  if (!item) return <p>작품 정보를 찾을 수 없습니다.</p>;
+function WorkDescription({ artwork }) {
+  if (!artwork) return <p>작품 정보를 찾을 수 없습니다.</p>;
 
   return (
     <div className="art-description">
       <div
         className="art-dimensions"
         style={{
-          width: `${item.data.w * 3}px`,
-          height: `${item.data.h * 3}px`,
+          width: `${artwork.size_width * 3}px`,
+          height: `${artwork.size_height * 3}px`,
         }}
       >
         작품 치수
         <div className="width">
-          {item.data.w}cm
-          <div className="width-arrow" style={{ width: `${item.data.w * 3}px` }} />
+          {artwork.sizeWidth}cm
+          <div className="width-arrow" style={{ width: `${artwork.sizeWidth * 3}px` }} />
         </div>
         <div className="height">
-          {item.data.h}cm
-          <div className="height-arrow" style={{ height: `${item.data.h * 3}px` }} />
+          {artwork.sizeHeight}cm
+          <div className="height-arrow" style={{ height: `${artwork.sizeHeight * 3}px` }} />
         </div>
       </div>
       <h2>CONDITION REPORT</h2>
-      <p>{item.data.condition}</p>
+      <p>{artwork.pictureCondition}</p>
     </div>
   );
 }
