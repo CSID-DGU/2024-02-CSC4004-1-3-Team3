7399bdae565645541305fdc956cd99f37f856f04
surl <msms1125@naver.com>
2024-11-26 17:58:46 +0900

    로그인 페이지 수정

--- Team3_front/front/src/pages/SignUp/SignUpStep2.jsx
+++ Team3_front/front/src/pages/SignUp/SignUpStep2.jsx
@@ -17,10 +17,8 @@ function SignUpStep2() {
 
   return (
     <div className="signup-page">
-      {/* 제목 변경 */}
-      <h2>{isPersonalMember ? '개인회원 가입' : '예술가 회원 가입'}</h2>
+      {/* 회원 유형 선택 탭 */}
       <div className="signup-tabs">
-        {/* 버튼 활성화 스타일 추가 */}
         <button className={`tab ${isPersonalMember ? 'active' : ''}`} onClick={handlePersonalClick}>
           개인 회원
         </button>
@@ -28,6 +26,8 @@ function SignUpStep2() {
           예술가 회원
         </button>
       </div>
+
+      {/* 회원가입 폼 */}
       <form className="signup-form">
         <label>
           아이디
@@ -41,14 +41,34 @@ function SignUpStep2() {
           이메일
           <input type="email" placeholder="email@example.com" />
         </label>
-        <div className="terms">
-          <input type="checkbox" id="agree-all" />
-          <label htmlFor="agree-all">전체 동의</label>
-          <input type="checkbox" id="agree-1" />
-          <label htmlFor="agree-1">(필수) 개인정보 약관에 동의</label>
-          <input type="checkbox" id="agree-2" />
-          <label htmlFor="agree-2">(선택) 마케팅 정보 수신 동의</label>
+
+        {/* 약관 동의 섹션 */}
+        <div className="terms-container">
+          <h3 className="terms-title">약관</h3>
+          <div className="terms-box">
+            <div className="terms-item">
+              <input type="checkbox" id="agree-all" />
+              <label htmlFor="agree-all">
+                <strong>전체 동의</strong>
+                <p>마케팅 정보 수신 동의를 포함합니다.</p>
+              </label>
+            </div>
+            <div className="terms-item">
+              <input type="checkbox" id="agree-1" />
+              <label htmlFor="agree-1">(필수) 개인회원 약관에 동의</label>
+            </div>
+            <div className="terms-item">
+              <input type="checkbox" id="agree-2" />
+              <label htmlFor="agree-2">(필수) 개인정보 수집 및 이용에 동의</label>
+            </div>
+            <div className="terms-item">
+              <input type="checkbox" id="agree-3" />
+              <label htmlFor="agree-3">(선택) 마케팅 정보 수신 동의</label>
+            </div>
+          </div>
         </div>
+
+        {/* 회원가입 버튼 */}
         <button type="submit" className="signup-btn">
           회원가입 완료
         </button>
