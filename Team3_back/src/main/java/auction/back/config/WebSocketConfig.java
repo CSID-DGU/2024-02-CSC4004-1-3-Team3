ceb771837f0b624ec2accfb3b2b72c1193690c9f
Kimd0ng <kdongh0406@gmail.com>
2024-12-03 20:32:35 +0900

    :ambulance: fix: config버그 수정

--- Team3_back/src/main/java/auction/back/config/WebSocketConfig.java
+++ Team3_back/src/main/java/auction/back/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final AuctionWebSocketHandler auctionWebSocketHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(auctionWebSocketHandler, "/auction/**")
+        registry.addHandler(auctionWebSocketHandler, "/auction/*")
                 .setAllowedOrigins("https://web-auction-m3egx220d3e063ff.sel4.cloudtype.app")
                 .withSockJS()
                 .setClientLibraryUrl("/sockjs-client.js")
