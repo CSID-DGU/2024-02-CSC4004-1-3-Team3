3c6eb160dc7090d68296245902c6d6b8daa8ade7
Kimd0ng <kdongh0406@gmail.com>
2024-12-03 17:31:24 +0900

    :sparkles: feat: webconfig 수정

--- Team3_back/src/main/java/auction/back/config/WebConfig.java
+++ Team3_back/src/main/java/auction/back/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry ){
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedOriginPatterns("*")
                 .allowedMethods("PATCH","GET","POST","PUT","DELETE","HEAD","OPTIONS")
                 .allowedHeaders("*")
