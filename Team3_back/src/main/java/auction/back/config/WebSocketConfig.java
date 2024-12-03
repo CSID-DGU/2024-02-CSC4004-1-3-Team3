8b3d6bc171f9c024f0790a0ffd2b3b95f09b253e
Kimd0ng <kdongh0406@gmail.com>
2024-12-03 14:46:54 +0900

    :sparkles: feat: websocket api 수정

--- Team3_back/src/main/java/auction/back/config/WebSocketConfig.java
+++ Team3_back/src/main/java/auction/back/config/WebSocketConfig.java
@@ -8,9 +8,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    private final AuctionWebSocketHandler auctionWebSocketHandler;
+
+    public WebSocketConfig(AuctionWebSocketHandler auctionWebSocketHandler) {
+        this.auctionWebSocketHandler = auctionWebSocketHandler;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new AuctionWebSocketHandler(), "/auction")
-                .setAllowedOrigins("*");
+        registry
+                .addHandler(auctionWebSocketHandler, "/auction")
+                .setAllowedOrigins("*")
+                .withSockJS();  // SockJS 폴백 지원 추가
     }
 }
